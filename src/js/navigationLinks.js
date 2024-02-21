188eca69f543edd004ff9289714b07a8f6c1b356
khaldoun36 <khaldoun36@gmail.com>
2024-02-21 20:27:19 +0400

    minor/added vercel adapter

---
+++ src/js/navigationLinks.js
@@ -0,0 +1,16 @@
+const navigationLinks = [
+    {
+        text: "About me",
+        slug: "./about",
+    },
+    {
+        text: "Insights",
+        slug: "./insights",
+    },
+    {
+        text: "Free resources",
+        slug: "./free-resources",
+    },
+];
+
+export default navigationLinks;
