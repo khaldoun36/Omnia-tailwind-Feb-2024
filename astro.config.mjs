b24261285ba3203881fe9aecd58e5c0e9b12106c
khaldoun36 <khaldoun36@gmail.com>
2024-03-11 09:57:15 +0400

    env variable/3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import vercel from "@astrojs/vercel/serverless";
 import storyblok from "@storyblok/astro";
 // import { loadEnv } from "vite";
 // const env = loadEnv("", process.cwd(), "STORYBLOK");
-const STORYBLOK_TOKEN = "TeKuT4VuYc6ykJD8KjUGAQtt";
+const MY_STORYBLOK_TOKEN = "TeKuT4VuYc6ykJD8KjUGAQtt";
 // https://astro.build/config
 export default defineConfig({
     integrations: [
         tailwind(),
         icon(),
         storyblok({
-            accessToken: STORYBLOK_TOKEN,
+            accessToken: MY_STORYBLOK_TOKEN,
             components: {
                 blogPost: "stroyblok/BlogPost",
                 blogPostList: "stroyblok/BlogPostList",
