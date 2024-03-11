35528b600e70198348d2a371b89c1efb877622b5
khaldoun36 <khaldoun36@gmail.com>
2024-03-11 10:09:33 +0400

    env variables issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 import storyblok from "@storyblok/astro";
-// import { loadEnv } from "vite";
-// const env = loadEnv("", process.cwd(), "STORYBLOK");
-const MY_STORYBLOK_TOKEN = "TeKuT4VuYc6ykJD8KjUGAQtt";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+const STORYBLOK_TOKEN = "TeKuT4VuYc6ykJD8KjUGAQtt";
 // https://astro.build/config
 export default defineConfig({
     integrations: [
         tailwind(),
         icon(),
         storyblok({
-            accessToken: MY_STORYBLOK_TOKEN,
+            accessToken: env.STORYBLOK_TOKEN,
+            bridge: env.STORYBLOK_IS_PREVIEW === "yes",
             components: {
                 blogPost: "stroyblok/BlogPost",
                 blogPostList: "stroyblok/BlogPostList",
@@ -21,7 +23,8 @@ export default defineConfig({
         }),
     ],
     output: "server",
-    adapter: vercel({
-        imageService: true,
-    }),
+    // adapter: vercel({
+    //     imageService: true,
+    // }),
+    adapter: vercel(),
 });
