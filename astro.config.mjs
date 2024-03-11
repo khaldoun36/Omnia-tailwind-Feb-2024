46d395889f7378cbf30779dba74c685d550d6170
khaldoun36 <khaldoun36@gmail.com>
2024-03-11 09:43:37 +0400

    FIXED ENV VAR TRIAL 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
-
 import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
-
 const env = loadEnv("", process.cwd(), "STORYBLOK");
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [
